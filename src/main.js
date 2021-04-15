950e0f0cd08250a21aefc2ea36b63ab5047dc380
Willow <wtschudy@ucsc.edu>
2021-04-14 17:35:41 -0700

    Added assets and Rocket class.

--- src/main.js
+++ src/main.js
@@ -4,4 +4,10 @@ let config = {
     height: 480,
     scene: [ Menu, Play ]
 }
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
 let game = new Phaser.Game(config);
+//set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
