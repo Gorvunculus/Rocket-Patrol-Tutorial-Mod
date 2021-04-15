54538a107f0bd94f75e8b9cb9c23ae1ed36601ca
Willow <wtschudy@ucsc.edu>
2021-04-14 18:43:56 -0700

    Added explosion animation and sprite reseting

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -19,11 +19,18 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
 
         if(Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+        }
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
             this.y -= this.moveSpeed;
         }
-        if(this.y <= borderUISize *3 + borderPadding) {
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
+        if(this.y <= borderUISize * 3 + borderPadding) {
+            this.reset()
         }
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize  - borderPadding;
     }
 }
